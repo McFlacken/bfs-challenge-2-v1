e176e5ea54a9461816c68a89a8190f9da8837ba6
McFlacken <angel.i.martinez@gmail.com>
2020-07-10 01:55:14 +0000

    First commit, app is ugly but it works

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+	   res.redirect("/home");
+});
+
+app.get("/home", function(req, res){
+
+	request('https://api.thecatapi.com/v1/images/search', function(error, response, body){
+	if(!error && response.statusCode == 200){
+		var parsedData = JSON.parse(body);
+		res.render("home", {data: parsedData})
+		console.log(parsedData[0]["url"]);
+	}
+});
+	
+});
+
+app.listen(3000, function(){
+	console.log('In a galaxy far far away... CAT app has now started...');
+});
